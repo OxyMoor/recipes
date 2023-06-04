978b407f9b93ee9c9d38bd58e764d7a8255f39a1
SeriakovaOksana <hikki0019@gmail.com>
2023-06-04 13:20:46 +0300

    version 4

--- js/dishes.js
+++ js/dishes.js
@@ -4,31 +4,31 @@ const dishesInfo = [
         dishesList: 
             [
                 {
-                    imgSrc: '../img/dishes/breakfast/test-dish.jpg',
+                    imgSrc: './img/dishes/breakfast/test-dish.jpg',
                     title: 'Сырники',
                     ingredientsList: [{ingr: 'творог 5%', quant: '400 г'}, {ingr: 'желток', quant: '2 шт.'}, {ingr: 'мука', quant: '2-3 ст.л.'}, {ingr: 'сахар', quant: 'по вкусу'}],
                     note: 'Тесто скатать в колбаску, обвалять в муке, разделить на сырники, каждый обвалять в муке. Жарить с обеих сторон.'
                 },
                 {
-                    imgSrc: '../img/dishes/breakfast/test-dish.jpg',
+                    imgSrc: './img/dishes/breakfast/test-dish.jpg',
                     title: 'Овсяноблин',
                     ingredientsList: [{ingr: 'овсянка', quant: '3 ст.л.'}, {ingr: 'яйцо', quant: '1 шт.'}, {ingr: 'молоко', quant: 'на глаз'}, {ingr: 'соль', quant: 'по вкусу'}],
                     note: 'Овсянку залить молоком, дать набухнуть. Добавить яйцо, соль. Жарить на медленном огне с обеих сторон. Сверху полить йогуртом, добавить банан, семечки.'
                 },
                 {
-                    imgSrc: '../img/dishes/breakfast/test-dish.jpg',
+                    imgSrc: './img/dishes/breakfast/test-dish.jpg',
                     title: 'Вареники с творогом',
                     ingredientsList: [{ingr: 'вареники', quant: '1 упаковка'}],
                     note: 'Поместить в кипящую подсоленную воду, варить 10 минут.'
                 },
                 {
-                    imgSrc: '../img/dishes/breakfast/test-dish.jpg',
+                    imgSrc: './img/dishes/breakfast/test-dish.jpg',
                     title: 'Овсянка с фруктами и орехами',
                     ingredientsList: [{ingr: 'овсянка', quant: '3 ст.л.'}, {ingr: 'молоко', quant: 'на глаз'}, {ingr: 'соль', quant: 'по вкусу'}, {ingr: 'фрукты', quant: 'по вкусу'}, {ingr: 'орехи, семмечки', quant: 'по вкусу'}],
                     note: 'Овсянку залить молоком, добавить соль, фрукты, орешки. Поместить в микроволновку на 50 сек.'
                 },
                 {
-                    imgSrc: '../img/dishes/breakfast/test-dish.jpg',
+                    imgSrc: './img/dishes/breakfast/test-dish.jpg',
                     title: 'Блины с творогом',
                     ingredientsList: [{ingr: 'молоко', quant: '500 мл'}, {ingr: 'яйцо', quant: '2 шт.'}, {ingr: 'мука', quant: '150 г'}, {ingr: 'соль', quant: 'щепотка'}, {ingr: 'сахар', quant: '1 ч.л.'}, {ingr: 'масло растительное', quant: '2 ст.л.'}, {ingr: 'творог', quant: '300 г'}, {ingr: 'желток', quant: '1 шт.'}],
                     note: 'Молоко должно быть теплым. Смешать молоко, яйца, муку, соль, сахар, масло. Дать тесту постоять 30 мин. Для наинки смешать творог и желток. Завернуть блины и еще раз обжарить.'
@@ -40,19 +40,19 @@ const dishesInfo = [
         dishesList:
             [
                 {
-                    imgSrc: '../img/dishes/breakfast/test-dish.jpg',
+                    imgSrc: './img/dishes/breakfast/test-dish.jpg',
                     title: 'Сырники',
                     ingredientsList: [{ingr: 'творог 5%', quant: '400г'}, {ingr: 'желток', quant: '2шт'}, {ingr: 'мука', quant: '2-3ст.л.'}, {ingr: 'сахар', quant: 'по вкусу'}],
                     note: 'Тесто скатать в колбаску, обвалять в муке, разделить на сырники, каждый обвалять в муке. Жарить с обеих сторон.'
                 },
                 {
-                    imgSrc: '../img/dishes/breakfast/test-dish.jpg',
+                    imgSrc: './img/dishes/breakfast/test-dish.jpg',
                     title: 'Сырники',
                     ingredientsList: [{ingr: 'творог 5%', quant: '400г'}, {ingr: 'желток', quant: '2шт'}, {ingr: 'мука', quant: '2-3ст.л.'}, {ingr: 'сахар', quant: 'по вкусу'}],
                     note: 'Тесто скатать в колбаску, обвалять в муке, разделить на сырники, каждый обвалять в муке. Жарить с обеих сторон.'
                 },
                 {
-                    imgSrc: '../img/dishes/breakfast/test-dish.jpg',
+                    imgSrc: './img/dishes/breakfast/test-dish.jpg',
                     title: 'Сырники',
                     ingredientsList: [{ingr: 'творог 5%', quant: '400г'}, {ingr: 'желток', quant: '2шт'}, {ingr: 'мука', quant: '2-3ст.л.'}, {ingr: 'сахар', quant: 'по вкусу'}],
                     note: 'Тесто скатать в колбаску, обвалять в муке, разделить на сырники, каждый обвалять в муке. Жарить с обеих сторон.'
@@ -65,19 +65,19 @@ const dishesInfo = [
         dishesList:
             [
                 {
-                    imgSrc: '../img/dishes/breakfast/test-dish.jpg',
+                    imgSrc: './img/dishes/breakfast/test-dish.jpg',
                     title: 'Сырники',
                     ingredientsList: [{ingr: 'творог 5%', quant: '400г'}, {ingr: 'желток', quant: '2шт'}, {ingr: 'мука', quant: '2-3ст.л.'}, {ingr: 'сахар', quant: 'по вкусу'}],
                     note: 'Тесто скатать в колбаску, обвалять в муке, разделить на сырники, каждый обвалять в муке. Жарить с обеих сторон.'
                 },
                 {
-                    imgSrc: '../img/dishes/breakfast/test-dish.jpg',
+                    imgSrc: './img/dishes/breakfast/test-dish.jpg',
                     title: 'Сырники',
                     ingredientsList: [{ingr: 'творог 5%', quant: '400г'}, {ingr: 'желток', quant: '2шт'}, {ingr: 'мука', quant: '2-3ст.л.'}, {ingr: 'сахар', quant: 'по вкусу'}],
                     note: 'Тесто скатать в колбаску, обвалять в муке, разделить на сырники, каждый обвалять в муке. Жарить с обеих сторон.'
                 },
                 {
-                    imgSrc: '../img/dishes/breakfast/test-dish.jpg',
+                    imgSrc: './img/dishes/breakfast/test-dish.jpg',
                     title: 'Сырники',
                     ingredientsList: [{ingr: 'творог 5%', quant: '400г'}, {ingr: 'желток', quant: '2шт'}, {ingr: 'мука', quant: '2-3ст.л.'}, {ingr: 'сахар', quant: 'по вкусу'}],
                     note: 'Тесто скатать в колбаску, обвалять в муке, разделить на сырники, каждый обвалять в муке. Жарить с обеих сторон.'
@@ -89,19 +89,19 @@ const dishesInfo = [
         dishesList:
             [
                 {
-                    imgSrc: '../img/dishes/breakfast/test-dish.jpg',
+                    imgSrc: './img/dishes/breakfast/test-dish.jpg',
                     title: 'Сырники',
                     ingredientsList: [{ingr: 'творог 5%', quant: '400г'}, {ingr: 'желток', quant: '2шт'}, {ingr: 'мука', quant: '2-3ст.л.'}, {ingr: 'сахар', quant: 'по вкусу'}],
                     note: 'Тесто скатать в колбаску, обвалять в муке, разделить на сырники, каждый обвалять в муке. Жарить с обеих сторон.'
                 },
                 {
-                    imgSrc: '../img/dishes/breakfast/test-dish.jpg',
+                    imgSrc: './img/dishes/breakfast/test-dish.jpg',
                     title: 'Сырники',
                     ingredientsList: [{ingr: 'творог 5%', quant: '400г'}, {ingr: 'желток', quant: '2шт'}, {ingr: 'мука', quant: '2-3ст.л.'}, {ingr: 'сахар', quant: 'по вкусу'}],
                     note: 'Тесто скатать в колбаску, обвалять в муке, разделить на сырники, каждый обвалять в муке. Жарить с обеих сторон.'
                 },
                 {
-                    imgSrc: '../img/dishes/breakfast/test-dish.jpg',
+                    imgSrc: './img/dishes/breakfast/test-dish.jpg',
                     title: 'Сырники',
                     ingredientsList: [{ingr: 'творог 5%', quant: '400г'}, {ingr: 'желток', quant: '2шт'}, {ingr: 'мука', quant: '2-3ст.л.'}, {ingr: 'сахар', quant: 'по вкусу'}],
                     note: 'Тесто скатать в колбаску, обвалять в муке, разделить на сырники, каждый обвалять в муке. Жарить с обеих сторон.'
@@ -113,19 +113,19 @@ const dishesInfo = [
         dishesList:
             [
                 {
-                    imgSrc: '../img/dishes/breakfast/test-dish.jpg',
+                    imgSrc: './img/dishes/breakfast/test-dish.jpg',
                     title: 'Сырники',
                     ingredientsList: [{ingr: 'творог 5%', quant: '400г'}, {ingr: 'желток', quant: '2шт'}, {ingr: 'мука', quant: '2-3ст.л.'}, {ingr: 'сахар', quant: 'по вкусу'}],
                     note: 'Тесто скатать в колбаску, обвалять в муке, разделить на сырники, каждый обвалять в муке. Жарить с обеих сторон.'
                 },
                 {
-                    imgSrc: '../img/dishes/breakfast/test-dish.jpg',
+                    imgSrc: './img/dishes/breakfast/test-dish.jpg',
                     title: 'Сырники',
                     ingredientsList: [{ingr: 'творог 5%', quant: '400г'}, {ingr: 'желток', quant: '2шт'}, {ingr: 'мука', quant: '2-3ст.л.'}, {ingr: 'сахар', quant: 'по вкусу'}],
                     note: 'Тесто скатать в колбаску, обвалять в муке, разделить на сырники, каждый обвалять в муке. Жарить с обеих сторон.'
                 },
                 {
-                    imgSrc: '../img/dishes/breakfast/test-dish.jpg',
+                    imgSrc: './img/dishes/breakfast/test-dish.jpg',
                     title: 'Сырники',
                     ingredientsList: [{ingr: 'творог 5%', quant: '400г'}, {ingr: 'желток', quant: '2шт'}, {ingr: 'мука', quant: '2-3ст.л.'}, {ingr: 'сахар', quant: 'по вкусу'}],
                     note: 'Тесто скатать в колбаску, обвалять в муке, разделить на сырники, каждый обвалять в муке. Жарить с обеих сторон.'
@@ -137,19 +137,19 @@ const dishesInfo = [
         dishesList:
             [
                 {
-                    imgSrc: '../img/dishes/breakfast/test-dish.jpg',
+                    imgSrc: './img/dishes/breakfast/test-dish.jpg',
                     title: 'Сырники',
                     ingredientsList: [{ingr: 'творог 5%', quant: '400г'}, {ingr: 'желток', quant: '2шт'}, {ingr: 'мука', quant: '2-3ст.л.'}, {ingr: 'сахар', quant: 'по вкусу'}],
                     note: 'Тесто скатать в колбаску, обвалять в муке, разделить на сырники, каждый обвалять в муке. Жарить с обеих сторон.'
                 },
                 {
-                    imgSrc: '../img/dishes/breakfast/test-dish.jpg',
+                    imgSrc: './img/dishes/breakfast/test-dish.jpg',
                     title: 'Сырники',
                     ingredientsList: [{ingr: 'творог 5%', quant: '400г'}, {ingr: 'желток', quant: '2шт'}, {ingr: 'мука', quant: '2-3ст.л.'}, {ingr: 'сахар', quant: 'по вкусу'}],
                     note: 'Тесто скатать в колбаску, обвалять в муке, разделить на сырники, каждый обвалять в муке. Жарить с обеих сторон.'
                 },
                 {
-                    imgSrc: '../img/dishes/breakfast/test-dish.jpg',
+                    imgSrc: './img/dishes/breakfast/test-dish.jpg',
                     title: 'Сырники',
                     ingredientsList: [{ingr: 'творог 5%', quant: '400г'}, {ingr: 'желток', quant: '2шт'}, {ingr: 'мука', quant: '2-3ст.л.'}, {ingr: 'сахар', quant: 'по вкусу'}],
                     note: 'Тесто скатать в колбаску, обвалять в муке, разделить на сырники, каждый обвалять в муке. Жарить с обеих сторон.'
